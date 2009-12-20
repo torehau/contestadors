deb520ec37ac667547cac325054a3f5a7d85d59e
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2009-12-20 22:23:16 +0100

    disabeling user sign up for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,21 @@ class UsersController < ApplicationController
 
   def new
     @user = Core::User.new
+    flash[:notice] = "Operation not yet supported"
   end
 
   def create
-#    @user = Core::User.new
-#    flash[:notice] = "Operation not yet supported"
-#    render :action => :new
+    @user = Core::User.new
+    flash[:notice] = "Operation not yet supported"
+    render :action => :new
     # TODO comment in when new users are allowed to sign up
-    @user = Core::User.new(params[:core_user])
-    if @user.save
-      flash[:notice] = "Account registered!"
-      redirect_back_or_default account_url
-    else
-      render :action => :new
-    end
+#    @user = Core::User.new(params[:core_user])
+#    if @user.save
+#      flash[:notice] = "Account registered!"
+#      redirect_back_or_default account_url
+#    else
+#      render :action => :new
+#    end
   end
 
   def show
