b47191fc8b88f18da3c93321923174f7219fae51
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2009-12-31 16:48:56 +0100

    added attributes for scores and starship operator

--- app/models/predictable/championship/match.rb
+++ app/models/predictable/championship/match.rb
@@ -1,10 +1,19 @@
 module Predictable
   module Championship
     class Match < ActiveRecord::Base
+      include Comparable
       set_table_name("predictable_championship_matches")
       belongs_to :stage, :class_name => "Predictable::Championship::Stage", :foreign_key => "predictable_championship_stage_id"
       belongs_to :home_team, :class_name => "Predictable::Championship::Team", :foreign_key => 'home_team_id'
       belongs_to :away_team, :class_name => "Predictable::Championship::Team", :foreign_key => 'away_team_id'
+
+      attr_accessor :home_team_score, :away_team_score
+
+      def <=> (other)
+        date_compare = self.play_date <=> other.play_date
+        return date_compare unless date_compare == 0
+        self.home_team.name <=> other.home_team.name
+      end
     end
   end
 end
