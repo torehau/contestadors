1d5c858e7c96e75bc848744fd38c7f051c60e63f
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2009-12-31 17:13:11 +0100

    added routes for predictions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
+  map.resources :predictions, :controller => "predictable/championship/predictions", 
+    :path_prefix => '/championship/:collection_type/:collection_id'
+  map.championship_predictions "championship_predictions",
+    :controller => "predictable/championship/predictions", :action => "new",
+    :collection_type => "group", :collection_id => "A"
   map.root :controller => "user_sessions", :action => "new"
 end
