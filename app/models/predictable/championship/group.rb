cae05497f746902ba4df93e48407d59de0293df8
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2009-12-31 16:47:30 +0100

    added matches method

--- app/models/predictable/championship/group.rb
+++ app/models/predictable/championship/group.rb
@@ -4,6 +4,10 @@ module Predictable
       set_table_name("predictable_championship_groups")
       has_many :table_positions, :class_name => "Predictable::Championship::GroupTablePosition", :foreign_key => "predictable_championship_group_id"
       has_many :teams, :through => :table_positions, :class_name => "Predictable::Championship::Team"
+
+      def matches
+        teams.collect {|t| t.matches}.flatten.uniq.sort
+      end
     end
   end
 end
