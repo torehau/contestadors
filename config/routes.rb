dbf7ebf62e9479234cf16997e71f540a94547c7f
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-01-03 22:34:41 +0100

    renamed route parameters

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :user_session
   map.resources :predictions, :controller => "predictable/championship/predictions", 
-    :path_prefix => '/championship/:collection_type/:collection_id'
+    :path_prefix => '/championship/:aggregate_root_type/:aggregate_root_id'
   map.championship_predictions "championship_predictions",
     :controller => "predictable/championship/predictions", :action => "new",
-    :collection_type => "group", :collection_id => "A"
+    :aggregate_root_type => "group", :aggregate_root_id => "A"
   map.root :controller => "user_sessions", :action => "new"
 end
