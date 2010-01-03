c74b5c24c4c2ad61dc020570b83a292ed15701f6
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-01-03 22:25:16 +0100

    added has many relation definitions

--- app/models/core/user.rb
+++ app/models/core/user.rb
@@ -2,5 +2,10 @@ module Core
   class User < ActiveRecord::Base
     set_table_name("core_users")
     acts_as_authentic
+    has_many :predictions, :class_name => "Core::Prediction", :foreign_key => "core_user_id" do
+      def by_predictable_item(set)
+        find(:all, :conditions => {:configuration_predictable_item_id => set.predictable_items.collect{|pi| pi.id}}).group_by(&:configuration_predictable_item_id)
+      end
+    end
   end
 end
