9a8567381e58404aadf7535fb7f21a373f782363
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-01-18 00:03:06 +0100

    added filter methods on predictable_items

--- app/models/configuration/set.rb
+++ app/models/configuration/set.rb
@@ -1,7 +1,14 @@
 module Configuration
   class Set < ActiveRecord::Base
     set_table_name "configuration_sets"
-    has_many :predictable_items, :class_name => "Configuration::PredictableItem", :foreign_key => "configuration_set_id"
+    has_many :predictable_items, :class_name => "Configuration::PredictableItem", :foreign_key => "configuration_set_id" do
+      def by_predictable_id
+        find(:all).group_by(&:predictable_id)
+      end
+      def for_predictable(predictable_id)
+        find(:first, :conditions => {:predictable_id => predictable_id})
+      end
+    end
     has_many :included_objectives, :class_name => "Configuration::IncludedObjective", :foreign_key => "configuration_set_id"
     has_many :objectives, :through => :included_objectives, :class_name => "Configuration::Objective"
 
