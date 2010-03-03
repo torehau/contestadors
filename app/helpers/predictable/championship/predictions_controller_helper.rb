c1873df088f3323a2ab05254b25463b576ad430f
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-03 18:59:03 +0100

    cleaned up nesty conditional nastiness

--- app/helpers/predictable/championship/predictions_controller_helper.rb
+++ app/helpers/predictable/championship/predictions_controller_helper.rb
@@ -3,26 +3,47 @@ module Predictable::Championship::PredictionsControllerHelper
     "predictable/championship/predictions/#{type}"
   end
 
-  def successful_prediction_message(aggr_type, aggr_id, new_predictions, prediction_state)
-    message = "Predictions succesfully saved for "
+  def successful_prediction_message(aggr_type, aggr_id, new_predictions, wizard)#prediction_state)
+    message = ""
     
-    if aggr_type.eql?(:group)
-      message += "Group #{aggr_id}. "
-
-      if new_predictions == true
-
-        message += "You can edit these prediction, or continue to predict "
-
-        if ('a'...'h') === prediction_state
-          next_group = aggr_id.succ
-          message += "#{link_to('Group ' + next_group, new_prediction_path('group', next_group))}."
-        elsif 'h'.eql?(prediction_state)
-          message += "#{link_to('Round of 16', new_prediction_path('stage', 'round-of-16'))}."
-        end
-      end
-    elsif aggr_type.eql?(:stage)
-      message += "Stage #{aggr_id.gsub('-',' ').capitalize}. "
+    unless wizard
+      message = successful_prediction_message_for_guest_user
+    else
+      message = "Predictions succesfully saved for "
+      message += send("successful_#{aggr_type.to_s}_prediction_message".to_sym, aggr_id, new_predictions, wizard)
     end
     message
+  end
+
+  private
+
+  def successful_prediction_message_for_guest_user
+    "The provided predictions for Group A were valid, and gives the resulting group table seen below."
+  end
+
+  def successful_group_prediction_message(aggr_id, new_predictions, wizard)
+    message = "Group #{aggr_id}. "
+
+    if new_predictions == true
+      message += "You can edit these prediction, or continue to predict "
+      message += link_to_next_wizard_step(wizard)
+    end
+    message
+  end
+
+  def link_to_next_wizard_step(wizard)
+    message = ""
+
+    if ('a'...'h') === wizard.current_step
+      next_group = wizard.next_step.upcase
+      message += "#{link_to('Group ' + next_group, new_prediction_path('group', next_group))}."
+    elsif 'h'.eql?(wizard.current_step)
+      message += "#{link_to('Round of 16', new_prediction_path('stage', 'round-of-16'))}."
+    end
+    message
+  end
+
+  def successful_stage_prediction_message(aggr_id, new_predictions, wizard)
+    "Stage #{wizard.current_step.gsub('-',' ').capitalize}. "
   end
 end
