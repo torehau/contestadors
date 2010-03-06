83a9934f1f1614648b7f8c5487877e2db6313e01
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-06 23:17:42 +0100

    added contest request param to predictions resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :user_session
   map.resources :predictions, :controller => "predictable/championship/predictions", 
-    :path_prefix => '/championship/:aggregate_root_type/:aggregate_root_id'
+    :path_prefix => '/:contest/:aggregate_root_type/:aggregate_root_id'
   map.championship_predictions "championship_predictions",
     :controller => "predictable/championship/predictions", :action => "new",
-    :aggregate_root_type => "group", :aggregate_root_id => "A"
+    :contest => "championship", :aggregate_root_type => "group", :aggregate_root_id => "A"
   map.root :controller => "user_sessions", :action => "new"
 end
