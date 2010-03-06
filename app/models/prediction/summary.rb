1b4710db4d210f263cff0dd3672ff888ab2e211a
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-06 22:54:04 +0100

    Added belongs_to relation with Contest. Ensured that winner and third place predictions are deleted along with other stage predictions. Some refactorings of the wizard functionality.

--- app/models/prediction/summary.rb
+++ app/models/prediction/summary.rb
@@ -2,19 +2,18 @@ module Prediction
   class Summary < ActiveRecord::Base
     set_table_name("prediction_summaries")
     belongs_to :user, :class_name => "Core::User", :foreign_key => 'core_user_id'
+    belongs_to :contest, :class_name => "Configuration::Contest", :foreign_key => 'configuration_contest_id'
     has_many :predictions, :through => :user
 
     # wizard helpers
-    attr_accessor :current_step, :next_step
-    attr_accessor :groups_available_for_prediction, :is_knockout_stages_available_for_prediction
-    alias_method :is_knockout_stages_available_for_prediction?, :is_knockout_stages_available_for_prediction
-
+    attr_accessor :current_step, :next_step, :all_available_steps
 
     def after_initialize
       update_wizard
     end
 
     PROGRESS_WHEN_GROUPS_AND_ROUND_OF_16_PREDICTED  = 77
+    LAST_GROUP_ID                                   = 'h'
     KNOCKOUT_STAGES                                 = [:r, :q, :s, :fi, :t]
     KNOCKOUT_STAGE_ID_BY_STATE_NAME                 = {'r'  => 'round-of-16',
                                                        'q'  => 'quarter-finals',
@@ -63,10 +62,12 @@ module Prediction
     def update_wizard
       self.current_step = convert_to_wizard_step_id(state)
       self.next_step = convert_to_wizard_step_id(get_next_possible_advanced_state)
-      self.groups_available_for_prediction = []
-      last_available_group = KNOCKOUT_STAGE_ID_BY_STATE_NAME.has_value?(self.next_step) ? 'h' : self.next_step
-      ('a'..last_available_group).each{|group_name| self.groups_available_for_prediction << group_name}
-      self.is_knockout_stages_available_for_prediction = KNOCKOUT_STAGE_ID_BY_STATE_NAME.has_value?(self.next_step)
+      self.all_available_steps = collect_available_steps
+    end
+
+    def convert_to_wizard_step_id(state_name)
+      return state_name unless KNOCKOUT_STAGE_ID_BY_STATE_NAME.has_key?(state_name)
+      KNOCKOUT_STAGE_ID_BY_STATE_NAME[state_name]
     end
 
     def get_next_possible_advanced_state
@@ -76,20 +77,33 @@ module Prediction
       if possible_transitions.size == 1
         next_possible_state = possible_transitions.first.to
       elsif possible_transitions.size > 1
-        next_possible_state = possible_transitions.select{|pt| not pt.event.to_s.include?("predict_group")}.first.to
+        knockout_stage_transitions = possible_transitions.select{|pt| not pt.event.to_s.include?("predict_group")}
+        next_possible_state = knockout_stage_transitions.empty? ? nil : knockout_stage_transitions.first.to
       end
       next_possible_state
     end
 
-    def convert_to_wizard_step_id(state_name)
-      return state_name unless KNOCKOUT_STAGE_ID_BY_STATE_NAME.has_key?(state_name)
-      KNOCKOUT_STAGE_ID_BY_STATE_NAME[state_name]
-    end        
+    def collect_available_steps
+      steps = []
+      last_available_group = is_all_groups_predicted? ? LAST_GROUP_ID : self.next_step
+      ('a'..last_available_group).each{|group_name| steps << GroupWizardStep.new(group_name)}
+      steps << StageWizardStep.new(stage_permalink) if is_all_groups_predicted?
+      steps
+    end
 
+    def is_all_groups_predicted?
+      (KNOCKOUT_STAGE_ID_BY_STATE_NAME.has_value?(self.next_step) or not self.next_step)
+    end
+
+    def stage_permalink
+      self.next_step ? self.next_step : self.current_step
+    end
+        
     # any prediction for knockout stages should be invalidated if changing predictions
     # for groups after having started predicting the knockout stages
     def delete_knockout_stage_predictions
-      items = get_predictable_items_for_explicit_predicted_knockout_stages
+      items = get_predictable_items_for_predictions_to_delete
+      
       ActiveRecord::Base.transaction do
         Prediction::Base.delete_all(["core_user_id = ? and configuration_predictable_item_id in (?)", user.id, items])
         self.percentage_completed = PROGRESS_WHEN_GROUPS_AND_ROUND_OF_16_PREDICTED
@@ -97,12 +111,16 @@ module Prediction
       end
     end
 
-    def get_predictable_items_for_explicit_predicted_knockout_stages
+    def get_predictable_items_for_predictions_to_delete
       items = []
       Predictable::Championship::Stage.explicit_predicted_knockout_stages.each do |stage|
         set = Configuration::Set.find_by_description "Teams through to #{stage.description}"
         set.predictable_items.each {|item| items << item.id}
       end
+      set = Configuration::Set.find_by_description "Third Place Team"
+      items << set.predictable_items.first.id
+      set = Configuration::Set.find_by_description "Winner Team"
+      items << set.predictable_items.first.id
       items
     end
   end
