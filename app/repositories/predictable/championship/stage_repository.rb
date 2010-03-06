94d1da63200ffe16fab5f539e8391c2cb6c90a9b
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-06 23:05:40 +0100

    handling saving final and tp play-off winner teams, and also derivation of tp play-off teams from semi-final predictions

--- app/repositories/predictable/championship/stage_repository.rb
+++ app/repositories/predictable/championship/stage_repository.rb
@@ -15,6 +15,7 @@ module Predictable
 
       def get_aggregate_root(aggregate_root_id)
         aggregate_root_id ||="round-of-16"
+        aggregate_root_id = "final" if aggregate_root_id.eql?("third-place")
         Stage.from_permalink(aggregate_root_id)
       end
 
@@ -35,11 +36,15 @@ module Predictable
         {}
       end
 
-      def save_predictions_for_aggregate
-        stage_teams_by_id = set_teams_for_next_stage
-        @stage_teams_set = Configuration::Set.find_by_description "Teams through to #{@root.next.description}"
-        save_predictions_for_stage(stage_teams_by_id)
-        @user.prediction_summary.predict_stage(@root.description)        
+      def save_predictions_for_aggregate                
+        unless @root.description.eql?("Final")
+          stage_teams_by_id = set_teams_for_next_stage
+          @stage_teams_set = Configuration::Set.find_by_description "Teams through to #{@root.next.description}"
+          save_predictions_for_stage(stage_teams_by_id)
+          @summary.predict_stage(@root.description)
+        else
+          save_final_and_third_place_play_off_winners
+        end
         stage_from_existing_predictions
       end
 
@@ -67,6 +72,7 @@ module Predictable
 
       # saves the predicted stage teams for the current user.
       def save_predictions_for_stage(stage_teams_by_id)
+        # TODO is this needed here?
         Prediction::Base.transaction do
 
           save_predictions(@stage_teams_set.predictable_items, stage_teams_by_id) do |stage_team|
@@ -77,12 +83,50 @@ module Predictable
         end
       end
 
+      def save_final_and_third_place_play_off_winners
+        Prediction::Base.transaction do
+          save_match_winner(Match.find_by_description("Final"), "Winner Team", @root.description)
+          save_match_winner(@aggregate.associated, "Third Place Team", "Third Place")
+        end
+      end
+
+      def save_match_winner(match, set_descr, stage_descr)
+        winner = @winners[match.id]
+        winner_set = Configuration::Set.find_by_description set_descr
+        predictable_items = winner_set.predictable_items
+        # TODO duplication from save_predictions
+        existing_predictions_by_item_id = @user.predictions.for_items_by_item_id(predictable_items)
+        @new_predictions = (existing_predictions_by_item_id.nil? or existing_predictions_by_item_id.empty?)
+        item = predictable_items.first
+        prediction = @new_predictions ? Prediction::Base.new : existing_predictions_by_item_id[item.id].first
+        save_predicted_value(prediction, @new_predictions, item, winner.id.to_s)
+        update_prediction_progress(PERCENTAGE_COMPLETED_FOR_STAGE)
+        @summary.predict_stage(stage_descr)
+      end
+
       def stage_from_existing_predictions        
         result = KnockoutStageResolver.new(@user).predicted_stages        
-        @aggregate.all_predicted_roots = result[1]
-        @root = result[0]
+        @aggregate.all_predicted_roots = result[1]        
+        @root = result[0]                
         @aggregate.id = @root.permalink
+        resolve_third_place_play_off if is_semi_finals_predicted?
         @root
+      end
+
+      def is_semi_finals_predicted?
+        @aggregate.all_predicted_roots.size > 2
+      end
+
+      def resolve_third_place_play_off
+        semi_final_defeated_teams = []
+
+        semi_finals_stage = Predictable::Championship::Stage.find_by_description("Semi finals")
+        @aggregate.all_predicted_roots[semi_finals_stage.id].matches.each {|match| semi_final_defeated_teams << match.team_not_through_to_next_stage}
+
+        if semi_final_defeated_teams.size == 2
+          @aggregate.associated.home_team = semi_final_defeated_teams[0]
+          @aggregate.associated.away_team = semi_final_defeated_teams[1]
+        end
       end
     end
   end
