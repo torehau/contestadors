a539e01dc3e3db08131b5221794a8611568c7755
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-06 22:41:20 +0100

    added has_many relationship for prediction summaries, and class method for getting all available contests

--- app/models/configuration/contest.rb
+++ app/models/configuration/contest.rb
@@ -2,6 +2,12 @@ module Configuration
   class Contest < ActiveRecord::Base
     set_table_name "configuration_contests"    
     has_many :included_sets, :class_name => "Configuration::IncludedSet", :foreign_key => "configuration_contest_id"
-    has_many :sets, :through => :included_sets, :class_name => "Configuration::Set"    
+    has_many :sets, :through => :included_sets, :class_name => "Configuration::Set"
+    has_many :prediction_summaries, :class_name => "Prediction::Summary", :foreign_key => "prediction_summary_id"
+
+    def self.all_available
+      now = Time.now
+      find(:all, :conditions => ["available_from <= ? and participation_ends_at >= ?", now, now])
+    end
   end
 end
