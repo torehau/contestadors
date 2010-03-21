2dceb1f171ae439d0e78c1655fa9cba8d2464dbd
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-21 15:19:20 +0100

    UserSession moved from deleted Core module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   private
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
-      @current_user_session = Core::UserSession.find
+      @current_user_session = UserSession.find
     end
 
     def current_user
