4548e611dd60c3a80d82f6153ba384af7d6e453b
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-03-21 16:12:01 +0100

    updated to use new Predictions resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.resources :predictions, :controller => "predictable/championship/predictions", 
+  map.resources :predictions,
     :path_prefix => '/:contest/:aggregate_root_type/:aggregate_root_id'
-  map.championship_predictions "championship_predictions",
-    :controller => "predictable/championship/predictions", :action => "new",
+  map.championship_predictions "championship/group/A",
+    :controller => "predictions", :action => "new",
     :contest => "championship", :aggregate_root_type => "group", :aggregate_root_id => "A"
   map.root :controller => "user_sessions", :action => "new"
 end
