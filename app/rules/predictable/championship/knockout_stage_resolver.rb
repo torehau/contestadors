a989d86dc3c3d6dd50514d4a0a7119dc6b9c349c
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-04-19 13:28:31 +0200

    various refactorings, including returning a Predictable::Result from the predicted_stages method

--- app/rules/predictable/championship/knockout_stage_resolver.rb
+++ app/rules/predictable/championship/knockout_stage_resolver.rb
@@ -4,16 +4,15 @@ module Predictable
     class KnockoutStageResolver
       include Ruleby
 
-      def initialize(aggregate)
-        @aggregate = aggregate
+      def initialize(user)
         @stages = Predictable::Championship::Stage.knockout_stages
         @teams = Predictable::Championship::Team.find(:all)        
         @predictable_items = stage_predictable_items
-        @predictions = @aggregate.user.predictions.for_items(@predictable_items)
-        @third_place_play_off = @aggregate.associated
+        @predictions = user.predictions.for_items(@predictable_items)
+        @third_place_play_off = Predictable::Championship::Match.find_by_description("Third Place")#@aggregate.associated
       end
 
-      def predicted_stages
+      def predicted_stages(current_aggregate)
         @predicted_stages = {}
 
         engine :predicted_stage_matches do |e|
@@ -33,55 +32,62 @@ module Predictable
           e.match
         end
 
-        @aggregate.all_predicted_roots = @predicted_stages
-        
-        if @aggregate.is_editing_existing_predictions?
-          @aggregate.all_invalidated_roots = get_stages_invalidated_by_edit
-        end
+        result = Predictable::Result.new(current_aggregate, @predicted_stages, unpredicted_stages, invalidated_stages(current_aggregate))
+        result.all_roots = @stages
         resolve_third_place_play_off if is_semi_finals_predicted?
-        @aggregate
+        result.aggregates_associated(:third_place, @third_place_play_off)
+        result
       end
 
-      private
+    private
 
       def stage_predictable_items
         ["Stage Teams", "Specific Team"].collect{|category_descr| Configuration::Category.find_by_description(category_descr)}.collect{|category| category.predictable_items}.flatten
       end
 
-      def get_stages_invalidated_by_edit
-        invalidated_stage = []
-        current_stage = @aggregate.root
-
-        while current_stage.next do
-          invalidated_stage << current_stage.next.id
-          current_stage = current_stage.next
-        end
-        invalidated_stage
+      def unpredicted_stages
+        unpredicted = {}
+        @stages.each {|stage| unpredicted[stage.id] = stage unless @predicted_stages.has_key?(stage.id)}
+        unpredicted
       end
 
-      def last_predicted_stage
+      def invalidated_stages(current_aggregate)
+        invalidated_stages = {}
+        current_stage = current_aggregate.root
 
-        for stage in @predicted_stages.values
-          unless @predicted_stages.values.include?(stage.next)
-            return stage
-          end
+        while current_stage and current_stage.next do
+          next_stage = current_stage.next
+          invalidated_stages[next_stage.id] = next_stage if @predicted_stages.has_key?(next_stage.id)
+          current_stage = next_stage
         end
-        @predicted_stages.values.first
+        invalidated_stages
       end
 
+#      def last_predicted_stage
+#
+#        for stage in @predicted_stages.values
+#          unless @predicted_stages.values.include?(stage.next)
+#            return stage
+#          end
+#        end
+#        @predicted_stages.values.first
+#      end
+
+      # TODO ruleify
       def is_semi_finals_predicted?
-        @aggregate.all_predicted_roots.size > 2
+        @predicted_stages.size > 2
       end
 
+      # TODO ruleify
       def resolve_third_place_play_off
         semi_final_defeated_teams = []
 
         semi_finals_stage = Predictable::Championship::Stage.find_by_description("Semi finals")
-        @aggregate.all_predicted_roots[semi_finals_stage.id].matches.each {|match| semi_final_defeated_teams << match.team_not_through_to_next_stage}
+        @predicted_stages[semi_finals_stage.id].matches.each {|match| semi_final_defeated_teams << match.team_not_through_to_next_stage}
 
         if semi_final_defeated_teams.size == 2
-          @aggregate.associated.home_team = semi_final_defeated_teams[0]
-          @aggregate.associated.away_team = semi_final_defeated_teams[1]
+          @third_place_play_off.home_team = semi_final_defeated_teams[0]
+          @third_place_play_off.away_team = semi_final_defeated_teams[1]
         end
       end
     end
