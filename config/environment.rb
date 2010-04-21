ac282d2e0336677bd7506474f26769e33a5fea04
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-04-21 13:09:47 +0200

    updated version of rails and state_machine

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -29,7 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "fastercsv", :version => '1.5.0'
   config.gem "authlogic", :version => '2.1.3'
   config.gem "ruleby", :version => '0.6'
-  config.gem "state_machine", :version => '0.8.1'
+  config.gem "state_machine", :version => '0.9.0'
 #  config.gem "inherited_resources", :version => '0.9.5'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
