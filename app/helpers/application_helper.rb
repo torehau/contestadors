01264b918f9637556b9cf33f93ddb837f2a5f48e
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-04-21 13:13:59 +0200

    added footer_div_class method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def footer_div_class
+    if current_controller.eql?("predictions") and @aggregate_root_type.eql?("group")
+      "footer-1"
+    else
+      "footer-2"
+    end
+  end
 end
