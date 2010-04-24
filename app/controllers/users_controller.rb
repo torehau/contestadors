1fce2791a77f24b019a3665162b944f3e3fed484
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-04-24 14:34:30 +0200

    bug fiks, and always rendering edit form in change action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,11 +31,9 @@ class UsersController < ApplicationController
 
   def update
     @user = @current_user # makes our views "cleaner" and more consistent
-    if @user.update_attributes(params[:core_user])
-      flash[:notice] = "Account updated!"
-      redirect_to account_url
-    else
-      render :action => :edit
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Account updated!"      
     end
+    render :action => :edit
   end
 end
