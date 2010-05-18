e3469a8e0b12544ce28b2c98c8abbf039379ddfd
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-18 11:53:47 +0200

    loading the contest if user is signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  before_filter :set_context_from_request_params, :if => :current_user
+
+protected
+
+  def set_context_from_request_params
+    @contest = Configuration::Contest.find(:first)
+  end
 end
