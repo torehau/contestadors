ca365f807898b7ca33de642cca8279bac0ecb15c
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-18 12:07:03 +0200

    updated contest_tabnav_items method to check for participation end time and include tool tip

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -16,23 +16,40 @@ module ContestsHelper
   end
 
   def contest_tabnav_items
-    items = [{:name => "Participants",
-      :path => contest_path(:contest => @contest.permalink, :role => @role, :id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
-      :highlight_conditions => [{:controller => "contests", :action => "show"}]}]
+    items = []
+    
+    if after_contest_participation_ends
+      items << {:name => "Overview",
+        :tip => "Contest overview and updates",
+        :path => contest_path(:contest => @contest.permalink, :role => @role, :id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
+        :highlight_conditions => [{:controller => "contests", :action => "show"}]}
+    end
+    items << {:name => "Participants",
+      :tip => "All participants of the contest",
+      :path => contest_participants_path(:contest => @contest.permalink, :role => @role, :contest_id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
+      :highlight_conditions => [{:controller => "participants", :action => "index"}]}
   
     if current_user.is_admin_of?(@contest_instance)
-      items << {:name => "Invite",
-                :path => new_contest_invitation_path(:contest => @contest.permalink,  :role => "admin", :contest_id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
-                :highlight_conditions => [{:controller => "invitations", :action => "new"},
-                                          {:controller => "invitations", :action => "create"}]}
+
+      if before_contest_participation_ends
+        items << {:name => "Invite",
+                  :tip => "Invite people to join the contest",
+                  :path => new_contest_invitation_path(:contest => @contest.permalink,  :role => "admin", :contest_id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
+                  :highlight_conditions => [{:controller => "invitations", :action => "new"},
+                                            {:controller => "invitations", :action => "create"}]}
+      end
       items << {:name => "Invite History",
+                :tip => "A list of previously sent invitions",
                 :path => contest_invitations_path(:contest => @contest.permalink, :role => "admin", :contest_id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
                 :highlight_conditions => [{:controller => "invitations", :action => "index"}]}
 
-      items << {:name => "Edit Contest",
-                :path => edit_contest_path(:contest => @contest.permalink,  :role => "admin", :id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
-                :highlight_conditions => [{:controller => "contests", :action => "edit"},
-                                          {:controller => "contests", :action => "update"}]}
+      if before_contest_participation_ends
+        items << {:name => "Edit Contest",
+                  :tip => "Rename contest or update invitation message",
+                  :path => edit_contest_path(:contest => @contest.permalink,  :role => "admin", :id => @contest_instance.permalink, :uuid => @contest_instance.uuid),
+                  :highlight_conditions => [{:controller => "contests", :action => "edit"},
+                                            {:controller => "contests", :action => "update"}]}
+      end
     end
     items
   end
