c03a94a91840737f2cf7accacc17739c04af7669
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-20 17:50:37 +0200

    rolled back last errouneous config setting

--- config/environment.rb
+++ config/environment.rb
@@ -62,5 +62,4 @@ Rails::Initializer.run do |config|
   ENV['RECAPTCHA_PRIVATE_KEY'] = '6LeLJroSAAAAAP3YdLKNsxLJEBNhNi7rU0rG45fT'
 
   config.action_mailer.default_url_options = {:host => 'www.contestadors.com'}
-  ActionMailer::Base.default_content_type = "text/html"
 end
