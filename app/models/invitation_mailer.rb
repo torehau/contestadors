1daf61086dab950e96f34e8bad9245696be043cd
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-20 16:38:56 +0200

    init rev

---
+++ app/models/invitation_mailer.rb
@@ -0,0 +1,21 @@
+class InvitationMailer < ActionMailer::Base
+  
+  def invite_existing_user(invitation)
+    invitation_message(invitation)
+  end
+
+  def invite_new_user(invitation)
+    invitation_message(invitation)
+  end
+
+private
+
+  def invitation_message(invitation)
+    subject    "#{invitation.sender.name} invites you to join a prediction contest at Contestadors"
+    recipients invitation.email
+    from       'no-reply@contestadors.com'
+    sent_on    Time.now
+
+    body       :invitation => invitation
+  end
+end
---
+++ app/models/invitation_observer.rb
@@ -0,0 +1,11 @@
+class InvitationObserver < ActiveRecord::Observer
+  
+  def after_create(invitation)
+    if invitation.existing_user
+      InvitationMailer.deliver_invite_existing_user(invitation)
+    else
+      # TODO include in due time
+#      InvitationMailer.deliver_invite_new_user(invitation)
+    end
+  end
+end
