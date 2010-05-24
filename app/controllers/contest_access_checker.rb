8cbd81b384e31d18b78af596efacc5129ecbb6b4
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-24 14:06:54 +0200

    contest id must be casted to string

--- app/controllers/contest_access_checker.rb
+++ app/controllers/contest_access_checker.rb
@@ -5,7 +5,7 @@ module ContestAccessChecker
     unless current_user.is_admin_of?(@contest_instance)
       unless current_user.is_participant_of?(@contest_instance)
 
-        unauthorized = @contest_instance.id + ", " + @contest_instance.uuid
+        unauthorized = @contest_instance.id.to_s + ", " + @contest_instance.uuid
         @contest_instance = selected_contest
         unless current_user.is_admin_of?(@contest_instance)
           raise current_user.name + " attempted to access unauthorized contest: " + unauthorized
