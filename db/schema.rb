4e1b64e085dfac4b3004588de974983f800938c5
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-25 00:30:00 +0200

    updated schema with perishable token for user

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100520084256) do
+ActiveRecord::Schema.define(:version => 20100524195631) do
 
   create_table "configuration_categories", :force => true do |t|
     t.string   "description"
@@ -238,21 +238,23 @@ ActiveRecord::Schema.define(:version => 20100520084256) do
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                            :null => false
-    t.string   "name",                             :null => false
-    t.string   "crypted_password",                 :null => false
-    t.string   "password_salt",                    :null => false
-    t.string   "persistence_token",                :null => false
-    t.integer  "login_count",       :default => 0, :null => false
+    t.string   "email",                             :null => false
+    t.string   "name",                              :null => false
+    t.string   "crypted_password",                  :null => false
+    t.string   "password_salt",                     :null => false
+    t.string   "persistence_token",                 :null => false
+    t.integer  "login_count",       :default => 0,  :null => false
     t.datetime "last_request_at"
     t.datetime "last_login_at"
     t.datetime "current_login_at"
     t.string   "last_login_ip"
     t.string   "current_login_ip"
+    t.string   "perishable_token",  :default => "", :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["last_request_at"], :name => "index_users_on_last_request_at"
+  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
   add_index "users", ["persistence_token"], :name => "index_users_on_persistence_token"
 
 end
