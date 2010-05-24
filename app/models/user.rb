59f2fc32f56f21b7ed79e6696dd0697f3ee60ec6
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-25 00:27:20 +0200

    added method for resetting passord

--- app/models/user.rb
+++ app/models/user.rb
@@ -145,6 +145,11 @@ class User < ActiveRecord::Base
     self.participations.as_participant(contest).collect{|participation| participation.contest_instance}
   end
 
+  def deliver_password_reset_instructions!
+    reset_perishable_token!
+    Notifier.deliver_password_reset_instructions(self)
+  end
+
 protected
 
   def after_create
