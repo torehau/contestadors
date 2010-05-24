cf4c47706006db6a11e4e802df2f3f114c529d43
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-24 13:22:41 +0200

    passing ekseption to handle_faulty_url method with info about the requesting client

--- app/controllers/rescue_controller.rb
+++ app/controllers/rescue_controller.rb
@@ -1,6 +1,10 @@
 class RescueController < ApplicationController
 
   def index
-    handle_faulty_url
+    msg = "Unknown path: "
+    msg += " host => " + request.host if request.host
+    msg += " host => " + request.path if request.path
+    msg += " host => " + request.env['HTTP_REFERER'] if request.env['HTTP_REFERER']
+    handle_faulty_url(Exception.new(msg))
   end
 end
