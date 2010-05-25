1b280973d5514e4f554b3901b4709ec520becb3b
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-25 12:49:55 +0200

    corrected some typo's

--- app/controllers/rescue_controller.rb
+++ app/controllers/rescue_controller.rb
@@ -3,8 +3,8 @@ class RescueController < ApplicationController
   def index
     msg = "Unknown path: "
     msg += " host => " + request.host if request.host
-    msg += " host => " + request.path if request.path
-    msg += " host => " + request.env['HTTP_REFERER'] if request.env['HTTP_REFERER']
+    msg += " path => " + request.path if request.path
+    msg += " http referer => " + request.env['HTTP_REFERER'] if request.env['HTTP_REFERER']
     handle_faulty_url(Exception.new(msg))
   end
 end
