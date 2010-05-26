501f9399483a018e24347f228c548ed201485db5
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-26 17:55:03 +0200

    added has_one association with the new score_table_positions table, and creates this instance in the after_create callback

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,6 +2,7 @@ class Participation < ActiveRecord::Base
   belongs_to :user
   belongs_to :contest_instance
   belongs_to :invitation
+  has_one :score_table_position
   validates_presence_of :user_id, :contest_instance_id
   validates_uniqueness_of :user_id, :scope => :contest_instance_id
   define_statistic :participants_count, :count => :all, :filter_on => {:active => 'active = ?', :contest_instance_id => 'contest_instance_id = ?'}
@@ -10,5 +11,10 @@ class Participation < ActiveRecord::Base
     if self.invitation
       self.invitation.accept!
     end
+    ScoreTablePosition.create!(:participation_id => self.id,
+                               :contest_instance_id => self.contest_instance.id,
+                               :prediction_summary_id => self.user.summary_of(self.contest_instance.contest).id,
+                               :user_id => self.user.id,
+                               :position => 1)
   end  
 end
