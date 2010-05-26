b1f610cf91e37c433ad85606bfbf3d3cc97736a9
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-26 21:25:01 +0200

    table sort order changed to map before championship, also giving a flash message about its intent at this time

--- app/controllers/score_tables_controller.rb
+++ app/controllers/score_tables_controller.rb
@@ -8,10 +8,14 @@ class ScoreTablesController < ApplicationController
     @positions_grid = initialize_grid(ScoreTablePosition,
       :include => [:user, :prediction_summary],
       :conditions => {:contest_instance_id => @contest_instance.id},
-      :order => 'position',
-      :order_direction => 'desc',
+      :order => (before_contest_participation_ends ? 'prediction_summaries.map' : 'position'),
+      :order_direction => (before_contest_participation_ends ? 'desc' : 'asc'),
       :per_page => 10
     )
+
+    if before_contest_participation_ends
+      flash.now[:notice] = "The Score Table will be updated during the Championship, ranking the participants based on how well their predictions meet the actual results."
+    end
   end
 
 protected
