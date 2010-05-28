edda8eaa0ce6e2d22035bcfdfdc34f2ba87563f8
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-05-29 00:56:09 +0200

    added rule for another multi browser weekness detected during testing

--- app/rules/predictable/championship/knockout_stage_validator.rb
+++ app/rules/predictable/championship/knockout_stage_validator.rb
@@ -15,11 +15,15 @@ module Predictable
 
           engine :knockout_stages do |e|
             rulebook = KnockoutStageValidationRulebook.new(e)
-            rulebook.rules
+            rulebook.rules(stage.description)
             rulebook.errors = @errors
 
             e.assert stage
             e.assert summary
+
+            set = Configuration::Set.find_by_description "Teams through to " + stage.description
+            summary.user.predictions_for(set).each {|prediction| e.assert prediction}
+            stage.matches.each {|match| e.assert match.winner}
 
             e.match
           end
@@ -33,7 +37,7 @@ module Predictable
 
         attr_accessor :errors
 
-        def rules
+        def rules(stage_description)
 
           {"h" => ["Quarter finals", "Semi finals", "Final"],
            "r" => ["Semi finals", "Final"],
@@ -41,13 +45,24 @@ module Predictable
 
             stage_descriptions.each do |stage_descr|
 
-              rule :invalid_stage_transitions,
+              rule :invalid_stage_transitions, {:priority => 2},
                 [Predictable::Championship::Stage, :stage, m.description == stage_descr],
                 [PredictionSummary, :summary, m.state == current_state] do |v|
 
                    @errors[v[:stage].id] = "Not possible to predict the " + v[:stage].description + ". Predictions for previous knockout stages have been invalidated."
                  end
             end
+          end
+
+
+          rule :winner_teams_through_to_stage_being_predicted, {:priority => 1},
+            [Predictable::Championship::Team, :team,
+             {m.id => :winner_team_id}],
+            [:not, Prediction, :stage_team_prediction, 
+              m.predicted_value(:winner_team_id, &c{|pv, wtid| pv.eql?(wtid.to_s)})] do |v|
+
+            @errors[:winner_team_id] = "Not possible to predict the " + stage_description + ". Teams selected as winners not predicted through to this stage."
+            retract v[:team]
           end
         end
       end
