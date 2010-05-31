9b10dd889e3c5fd5876b180fa8ff2471c44926e1
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-01 01:19:14 +0200

    opened up for sending invitations to non users

--- app/models/invitation_observer.rb
+++ app/models/invitation_observer.rb
@@ -3,9 +3,8 @@ class InvitationObserver < ActiveRecord::Observer
   def after_create(invitation)
     if invitation.existing_user
       InvitationMailer.deliver_invite_existing_user(invitation)
-    else
-      # TODO include in due time
-#      InvitationMailer.deliver_invite_new_user(invitation)
+    else      
+      InvitationMailer.deliver_invite_new_user(invitation)
     end
     invitation.deliver!
   end
