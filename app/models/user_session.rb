ddc74552aecf37378393a443412bfa181136a611
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-01 01:20:47 +0200

    allowing authentication with RPX service

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,2 +1,12 @@
 class UserSession < Authlogic::Session::Base
+  rpx_key RPX_API_KEY
+
+private
+
+  def map_rpx_data
+    self.attempted_record.send("#{klass.email_field}=", @rpx_data['profile']['email'] ) if attempted_record.send(klass.email_field).blank?
+
+    # map some other columns explicitly
+    self.attempted_record.name = @rpx_data['profile']['displayName'] if attempted_record.name.blank?
+  end
 end
