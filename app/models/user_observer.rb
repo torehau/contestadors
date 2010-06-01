30d1028cf8a6ebdf8183b9408c099c2f83d3c223
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-01 04:11:15 +0200

    also sync pending invitations on user account updates

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,4 +2,8 @@ class UserObserver < ActiveRecord::Observer
   def after_create(user)
     Invitation.sync_existing_invitations_for(user)
   end
+
+  def after_update(user)
+    Invitation.sync_existing_invitations_for(user)
+  end
 end
