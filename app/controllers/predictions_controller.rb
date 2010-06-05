71db94cd35a8f8c9440ee86b1abb84399af2af09
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-05 15:37:43 +0200

    introduced more filters to check that a user is logged inn unless preview is available and also validating that a requested prediction state is available at this stage.

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,6 +1,9 @@
 class PredictionsController < ApplicationController
   before_filter :set_context_from_request_params
+  before_filter :require_contest
   before_filter :before_contest_participation_ends, :only => [:new, :create, :update]
+  before_filter :require_user, :unless => :preview_available
+  before_filter :prediction_state_available, :if => :current_user
 
   def index
     if before_contest_participation_ends
@@ -33,11 +36,12 @@ class PredictionsController < ApplicationController
 protected
 
   def set_context_from_request_params
-    @contest = Configuration::Contest.find_by_permalink(params[:contest])
-    @predictions_view_path = "predictable/#{@contest.permalink}/predictions/"
+    @contest = Configuration::Contest.from_permalink_or_first_available(params[:contest])
     @aggregate_root_type = params[:aggregate_root_type]
     @aggregate_root_id = params[:aggregate_root_id]
+    @requested_prediction_state = @contest.prediction_state_by_aggregate_root(@aggregate_root_type, @aggregate_root_id)
     @repository = @contest.repository(@aggregate_root_type, current_user)
+    @predictions_view_path = "predictable/#{@contest.permalink}/predictions/"
   end
 
   def set_wizard_and_progress_for_current_user
@@ -63,5 +67,19 @@ protected
   def get_aggregate_root_id
     return 'completed' if @wizard and @wizard.is_completed? and not @aggregate.has_validation_errors?
     @aggregate.root_id
+  end
+
+  def preview_available
+    return false unless @requested_prediction_state
+    @requested_prediction_state.preview_available?
+  end
+  
+  def prediction_state_available
+    next_available_prediction_state = current_user.next_available_prediction_state(@contest)
+    
+    if next_available_prediction_state.is_before?(@requested_prediction_state)
+      redirect_to new_prediction_path(next_available_prediction_state.request_params)
+      return false
+    end
   end
 end
