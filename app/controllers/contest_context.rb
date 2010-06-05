b0adbecd0572d262393754bf29ffaec0f37076e6
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-05 20:25:02 +0200

    selecting a resonable contest if request param was hampered with

--- app/controllers/contest_context.rb
+++ app/controllers/contest_context.rb
@@ -2,7 +2,7 @@ module ContestContext
   protected
 
   def set_contest_context(permalink, role, contest_id, contest_uuid)
-    @contest = Configuration::Contest.find_by_permalink(permalink)
+    @contest = Configuration::Contest.from_permalink_or_first_available(permalink)
     @role = role
     @contest_instance = ContestInstance.find_by_permalink_and_uuid(contest_id, contest_uuid)
   end
