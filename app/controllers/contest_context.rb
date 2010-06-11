eb98ef3d3dd1d55d92ebb86ee599b8820f25efc5
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-11 16:27:57 +0200

    new variable assignment for chekcing time

--- app/controllers/contest_context.rb
+++ app/controllers/contest_context.rb
@@ -3,6 +3,7 @@ module ContestContext
 
   def set_contest_context(permalink, role, contest_id, contest_uuid)
     @contest = Configuration::Contest.from_permalink_or_first_available(permalink)
+    @before_contest_participation_ends = before_contest_participation_ends
     @role = role
     @contest_instance = ContestInstance.find_by_permalink_and_uuid(contest_id, contest_uuid)
   end
