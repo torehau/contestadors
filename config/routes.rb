4888edf53e5d2ef86a9e868634cba005fc9eea76
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-11 16:56:05 +0200

    added routes for prediction overview pages

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,17 @@ ActionController::Routing::Routes.draw do |map|
   map.championship_predictions "championship/group/A",
     :controller => "predictions", :action => "new",
     :contest => "championship", :aggregate_root_type => "group", :aggregate_root_id => "A"
-#  map.user_predictions '/your/:contest/:aggregate_root_type/:aggregate_root_id/predictions', :controller => "predictions", :action => "show", :method => :get
+  map.user_predictions '/your/:contest/:aggregate_root_type/:aggregate_root_id/predictions', :controller => "predictions", :action => "show", :method => :get
   map.resources :contests, :path_prefix => '/:contest/:role' do |contests|
     contests.resources :invitations
     contests.resources :participants
     contests.resource :score_table
   end
+  map.upcoming_events '/:contest/:role/contests/:id/upcoming', :controller => "contests", :action => "upcoming_events"
+  map.latest_results '/:contest/:role/contests/:id/latest', :controller => "contests", :action => "latest_results"
   map.resources :invitations, :path_prefix => '/:contest',
     :collection => {:pending => :get, :accepted => :get}
-#  map.participant_predictions '/:contest/:role/:contest_id/participant-predictions', :controller => "participants", :action => "show"
+  map.participant_predictions '/:contest/:role/:contest_id/participant-predictions', :controller => "participants", :action => "show"
   map.home ':action', :controller => 'home', :action => /about|rules|terms|privacy|contact|faq/
   map.root :controller => "user_sessions", :action => "new"
   map.catch_all '*url', :controller => "rescue"
