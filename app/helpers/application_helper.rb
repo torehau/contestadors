0ceb5515c0fe14371cbdd67420a1598038932f01
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-11 16:34:52 +0200

    moved contest_instance_menu_link method to controller, and added prediction_summary_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,18 +12,17 @@ module ApplicationHelper
     "You must be signed in to access this page. Sign in with one of the options listed below to the right, or #{link_to('create a new Contestadors account ', new_account_path)}."
   end
 
-  def contest_instance_menu_link(contest_instance)
-    if before_contest_participation_ends
-      contest_participants_path(:contest => contest_instance.contest.permalink,
-        :role => contest_instance.role_for(current_user),
-        :contest_id => contest_instance.permalink,
-        :uuid => contest_instance.uuid)
-    else
-      contest_path(:contest => contest_instance.contest.permalink,
-        :role => contest_instance.role_for(current_user),
-        :id => contest_instance.permalink,
-        :uuid => contest_instance.uuid)
+  def prediction_summary_link(participation)
+    participation ||= current_user.participations.of(@contest_instance)
+    role ||= @contest_instance.role_for(current_user)
+    query_params = {:contest => @contest.permalink,
+                    :role => role, :contest_id => @contest_instance.permalink,
+                    :uuid => @contest_instance.uuid}
+
+    if participation and participation.invitation
+      query_params[:pid] = participation.invitation.token
     end
+    participant_predictions_path(query_params)
   end
 
   # put this in the body after a form to set the input focus to a specific control id
@@ -54,5 +53,22 @@ module ApplicationHelper
                     :before => "Element.show('spinner-#{object.id}')",
                     :after => "Element.hide('spinner-#{object.id}')",
                     :with => "this.name + '=' + this.checked")
+  end
+
+  def button_to_link(name, link, options={})
+    confirm_option = options.delete(:confirm)
+    popup_option = options.delete(:popup)
+    link_function = popup_option ? redirect_function(link,:new_window => true) : redirect_function(link)
+    link_function = "if (confirm('#{escape_javascript(confirm_option)}')) { #{link_function}; }" if confirm_option
+    button_to_function name, link_function, options
+  end
+
+  def redirect_function(location, options={})
+    location = location.is_a?(String) ? location : url_for(location)
+    if options[:new_window]
+      %|window.open('#{location}')|
+    else
+      %|{window.location.href='#{location}'}|
+    end
   end
 end
