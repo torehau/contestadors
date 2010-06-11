2e3af0cc114d79e4ae2b71647cd6c8b40a3de894
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-11 16:47:14 +0200

    made comparable

--- app/models/score_table_position.rb
+++ app/models/score_table_position.rb
@@ -1,6 +1,13 @@
 class ScoreTablePosition < ActiveRecord::Base
+  include Comparable
   belongs_to :participation
   belongs_to :prediction_summary
   belongs_to :contest_instance
   belongs_to :user
+
+  def <=> (other)
+    score_compare = other.prediction_summary.total_score <=> self.prediction_summary.total_score
+    return score_compare unless score_compare == 0
+    other.prediction_summary.map <=> self.prediction_summary.map
+  end
 end
