9d75b6865bf4264895a75b32df3740ef7875f3c0
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-11 16:36:41 +0200

    added objectives_meet_div_class method

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,2 +1,10 @@
 module ParticipantsHelper
+  def objectives_meet_div_class_for(predictable)
+    div_class = "objectives_meet"
+
+    if predictable.objectives_meet
+      div_class += "_" + predictable.objectives_meet.to_s
+    end
+    div_class
+  end
 end
