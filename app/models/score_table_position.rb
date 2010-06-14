1c8981a5f5bd23ef80bef7ed1baf2dc1a4a5e181
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-14 17:32:07 +0200

    deactivated users are sorted last

--- app/models/score_table_position.rb
+++ app/models/score_table_position.rb
@@ -6,8 +6,21 @@ class ScoreTablePosition < ActiveRecord::Base
   belongs_to :user
 
   def <=> (other)
+    active_compare = compate_active_participation(other)
+    return active_compare unless active_compare == 0
     score_compare = other.prediction_summary.total_score <=> self.prediction_summary.total_score
     return score_compare unless score_compare == 0
     other.prediction_summary.map <=> self.prediction_summary.map
+  end
+
+  private
+
+  def compate_active_participation(other)
+    return 0 if self.participation.active.eql?(other.participation.active)
+    if self.participation.active == true
+      -1
+    else
+      1
+    end
   end
 end
