a0ec4603ab48bab97121aec3eb51fbd8052aeb63
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-17 18:23:10 +0200

    added rules and category descr as input param to find_items method

--- app/rules/predictable/championship/predictable_items_resolver.rb
+++ app/rules/predictable/championship/predictable_items_resolver.rb
@@ -9,13 +9,13 @@ module Predictable
         @items_by_predictable_id = {}
       end
 
-      def find_items
+      def find_items(category_desc="Group Matches")
         engine :predictable_items_resolver do |e|
           rulebook = PredictableItemsRulebook.new(e)
           rulebook.items_by_predictable_id = @items_by_predictable_id
           rulebook.rules(@predictable_item_state)
           @preditables.each {|predictable| e.assert predictable}
-          Configuration::Category.find_by_description("Group Matches").predictable_items.each {|item| e.assert item}
+          Configuration::Category.find_by_description(category_desc).predictable_items.each {|item| e.assert item}
 
           e.match
         end
@@ -30,12 +30,14 @@ module Predictable
 
         def rules(predictable_item_state)
           group_matches_items_rule(predictable_item_state)
+          group_table_positions_items_rule(predictable_item_state)
+          stage_teams_items_rule(predictable_item_state)
         end
 
       private
 
         def group_matches_items_rule(predictable_item_state)
-           rule :group_matches_items,# {:priority => 4},
+           rule :group_matches_items, #{:priority => 3},
              [Predictable::Championship::Match, :group_match,
                {m.id => :group_match_id}],
              [Configuration::PredictableItem, :group_match_item,
@@ -45,6 +47,34 @@ module Predictable
                @items_by_predictable_id[v[:group_match].id] = v[:group_match_item]
                retract v[:group_match]
                retract v[:group_match_item]
+          end
+        end
+
+        def group_table_positions_items_rule(predictable_item_state)
+           rule :group_matches_items, #{:priority => 2},
+             [Predictable::Championship::GroupTablePosition, :group_table_position,
+               {m.id => :group_table_position_id}],
+             [Configuration::PredictableItem, :group_table_position_item,
+               m.state_name == predictable_item_state,
+               m.predictable_id == b(:group_table_position_id)] do |v|
+
+               @items_by_predictable_id[v[:group_table_position].id] = v[:group_table_position_item]
+               retract v[:group_table_position]
+               retract v[:group_table_position_item]
+          end
+        end
+
+        def stage_teams_items_rule(predictable_item_state)
+           rule :group_matches_items, #{:priority => 2},
+             [Predictable::Championship::StageTeam, :stage_team,
+               {m.id => :stage_team_id}],
+             [Configuration::PredictableItem, :stage_team_item,
+               m.state_name == predictable_item_state,
+               m.predictable_id == b(:stage_team_id)] do |v|
+
+               @items_by_predictable_id[v[:stage_team].id] = v[:stage_team_item]
+               retract v[:stage_team]
+               retract v[:stage_team_item]
           end
         end
       end
