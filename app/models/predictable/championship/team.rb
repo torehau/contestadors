3053f17ce22ca8bc4c83c40e7dcc6fc7b650ff9c
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-22 01:17:29 +0200

    added is_through_to_stage, objectives_meet and objectives_meet_for accessors

--- app/models/predictable/championship/team.rb
+++ app/models/predictable/championship/team.rb
@@ -4,19 +4,29 @@ module Predictable
       set_table_name("predictable_championship_teams")
       has_many :home_matches, :class_name => "Predictable::Championship::Match", :foreign_key => 'home_team_id'
       has_many :away_matches, :class_name => "Predictable::Championship::Match", :foreign_key => 'away_team_id'
-      has_many :stage_teams, :class_name => "Predictable::Championship::StageTeam", :foreign_key => "predictable_championship_team_id"
+      has_many :stage_teams, :class_name => "Predictable::Championship::StageTeam", :foreign_key => "predictable_championship_team_id" do
+        def through_to(stage)
+          find(:first, :conditions => {:predictable_championship_team_id => self.id, :predictable_championship_stage_id => stage.id})
+        end
+      end
       has_many :stages, :through => :stage_teams, :class_name => "Predictable::Championship::Stage"
       has_many :players, :class_name => "Predictable::Championship::Player", :foreign_key => "predictable_championship_team_id"
       has_one :group_table_position, :class_name => "Predictable::Championship::GroupTablePosition", :foreign_key => 'predictable_championship_team_id'
 
       attr_accessor :through_to_stage
+      attr_accessor :objectives_meet, :objectives_meet_for
       
       def after_initialize
         self.through_to_stage = []
+        self.objectives_meet_for = {}
       end
 
       def matches
         home_matches + away_matches
+      end
+
+      def is_through_to_stage?(stage)
+        self.stage_teams.through_to(stage)
       end
 
       def is_through_to_next_stage?(current_stage)
