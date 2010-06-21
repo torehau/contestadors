cdbe09e29d3d3579869d3265dad846ec569959a5
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-22 01:12:23 +0200

    changed input paramter from predictable to objectives meet

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,9 +1,9 @@
 module ParticipantsHelper
-  def objectives_meet_div_class_for(predictable)
+  def objectives_meet_div_class_for(objectives_meet)
     div_class = "objectives_meet"
 
-    if predictable.objectives_meet
-      div_class += "_" + predictable.objectives_meet.to_s
+    if objectives_meet
+      div_class += "_" + objectives_meet.to_s
     end
     div_class
   end
