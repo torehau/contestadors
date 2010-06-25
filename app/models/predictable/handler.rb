5e5b73690490807af877b8dcbc603217961d04e4
Huijiang Wang <quiomo@quiomo-laptop.(none)>
2010-06-25 20:50:39 +0200

    added new accessor and method

--- app/models/predictable/handler.rb
+++ app/models/predictable/handler.rb
@@ -1,9 +1,16 @@
 module Predictable
   module Handler
+    attr_accessor :objectives
     attr_accessor :objectives_meet
 
     def resolve_objectives_for(prediction, objectives)
       raise "Abstract method. Must be implemted by including class."
+    end
+
+    def total_possible_points
+      tpp = 0
+      @objectives.each{|o| tpp += o.possible_points}
+      tpp
     end
   end
 end
