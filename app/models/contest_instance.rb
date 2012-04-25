66b48a3e9d892010462ad0a654f5d2df9a1e0c46
Tore Hauge <tore@tore-VirtualBox.(none)>
2012-04-26 00:51:16 +0200

    Small fixes

--- app/models/contest_instance.rb
+++ app/models/contest_instance.rb
@@ -5,13 +5,13 @@ class ContestInstance < ActiveRecord::Base
   after_create :make_admin_first_participant
   belongs_to :contest, :class_name => "Configuration::Contest", :foreign_key => "configuration_contest_id"
   belongs_to :admin, :class_name => "User", :foreign_key => "admin_user_id"
-  has_many :invitations
-  has_many :participations do
+  has_many :invitations, :dependent => :destroy
+  has_many :participations, :dependent => :destroy do
     def active
       find(:all, :conditions => {:active => true})
     end
   end
-  has_many :score_table_positions
+  has_many :score_table_positions, :dependent => :destroy
   validates_presence_of :name, :admin_user_id, :configuration_contest_id
   validates_length_of :description, :maximum => 1000
 
@@ -29,35 +29,34 @@ class ContestInstance < ActiveRecord::Base
   end
 
   def role_for(user)
-    return "admin" if user and user.id and user.id.eql?(admin.id)
-    "member"
+    user and user.id and user.id.eql?(admin.id) ? "admin" : "member"
   end
 
   # ["Active participants: 1. Pending invitations:  0.", "Created at: " + instance.created_at.to_s(:short)]
-  def summary_for(user)
-    summaries = []
-    active_participants_count = Participation.get_stat(:participants_count, :active => 't', :contest_instance_id => self.id)
-    participants_summary = "Active participants: " + active_participants_count.to_s + ". "
-
-    if user.is_admin_of?(self)
-
-      if Time.now < self.contest.participation_ends_at
-        new_invitations_count = Invitation.get_stat(:contest_invitations_count,  :state => 'n', :contest_instance_id => self.id)
-        sent_invitations_count = Invitation.get_stat(:contest_invitations_count,  :state => 's', :contest_instance_id => self.id)
-        participants_summary += "New invitations: "  + (new_invitations_count + sent_invitations_count).to_s
-      else
-        deactivated_participants_count = Participation.get_stat(:participants_count, :active => 'f', :contest_instance_id => self.id)
-        participants_summary += "Deactivated participants: " + deactivated_participants_count.to_s
-      end
-      summaries << participants_summary
-      summaries << "Created on: " + self.created_at.to_s(:short)
-    else
-      summaries << "Admin: " + self.admin.name + ". " + participants_summary
-      participation = user.participations.of(self)
-      summaries << "Invitation accepted on: " + participation.created_at.to_s(:short)
-    end
-    summaries
-  end
+  #def summary_for(user)
+  #  summaries = []
+  #  active_participants_count = Participation.get_stat(:participants_count, :active => 't', :contest_instance_id => self.id)
+  #  participants_summary = "Active participants: " + active_participants_count.to_s + ". "
+  #
+  #  if user.is_admin_of?(self)
+  #
+  #    if Time.now < self.contest.participation_ends_at
+  #      new_invitations_count = Invitation.get_stat(:contest_invitations_count,  :state => 'n', :contest_instance_id => self.id)
+  #      sent_invitations_count = Invitation.get_stat(:contest_invitations_count,  :state => 's', :contest_instance_id => self.id)
+  #      participants_summary += "New invitations: "  + (new_invitations_count + sent_invitations_count).to_s
+  #    else
+  #      deactivated_participants_count = Participation.get_stat(:participants_count, :active => 'f', :contest_instance_id => self.id)
+  #      participants_summary += "Deactivated participants: " + deactivated_participants_count.to_s
+  #    end
+  #    summaries << participants_summary
+  #    summaries << "Created on: " + self.created_at.to_s(:short)
+  #  else
+  #    summaries << "Admin: " + self.admin.name + ". " + participants_summary
+  #    participation = user.participations.of(self)
+  #    summaries << "Invitation accepted on: " + participation.created_at.to_s(:short)
+  #  end
+  #  summaries
+  #end
 
   def active_participants
     self.participations.active.collect {|participation| participation.user }
