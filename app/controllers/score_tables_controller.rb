82a7f2df273a0d7dea9c1855aed931c1d218cc05
Tore Hauge <tore.hauge@outlook.com>
2014-05-28 00:08:45 +0200

    Disabeling score table before tournament starts

--- app/controllers/score_tables_controller.rb
+++ app/controllers/score_tables_controller.rb
@@ -8,16 +8,17 @@ class ScoreTablesController < ApplicationController
 
   def show
     save_to_session(@contest_instance)
-    @positions_grid = initialize_grid(ScoreTablePosition,
-      :include => [:user, :prediction_summary, :participation],
-      :conditions => {:contest_instance_id => @contest_instance.id, :participations => {:active => true}},
-      :order => (before_contest_participation_ends ? 'prediction_summaries.map' : 'position'),
-      :order_direction => (before_contest_participation_ends ? 'desc' : 'asc'),
-      :per_page => 30
-    )
 
-    if before_contest_participation_ends
+    if @before_contest_participation_ends
+      @score_table_not_available_message = "Score Table not available yet"
       flash.now[:notice] = "The Score Table will be updated during the #{@contest.name}, ranking the participants based on how well their predictions meet the actual results."
+    else
+	  @positions_grid = initialize_grid(ScoreTablePosition,
+	    :include => [:user, :prediction_summary, :participation],
+	    :conditions => {:contest_instance_id => @contest_instance.id, :participations => {:active => true}},
+	    :order => (before_contest_participation_ends ? 'prediction_summaries.map' : 'position'),
+        :order_direction => (before_contest_participation_ends ? 'desc' : 'asc'),
+	    :per_page => 30)    
     end
   end
 
