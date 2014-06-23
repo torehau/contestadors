52c5af3456fa44f13c0c835c78a80b6fa0e360a9
Tore Hauge <tore.hauge@outlook.com>
2014-06-24 01:45:17 +0200

    Fixing stage teams prediction displays

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -7,4 +7,18 @@ module ParticipantsHelper
     end
     div_class
   end
+  
+  def stage_objectives_meet_div_class_for(objectives_meet_by_stage_id, stage)
+    objectives_meet = objectives_meet_by_stage_id[stage.id]
+    
+    if objectives_meet
+      return "objectives_meet_" + objectives_meet.to_s
+    end
+    
+    while objectives_meet.nil? and stage.description != "Round of 16" do
+      stage = stage.previous
+      objectives_meet = objectives_meet_by_stage_id[stage.id]
+    end
+    (!objectives_meet.nil? and objectives_meet == 0) ? "objectives_meet_0" : "objectives_meet"  
+  end
 end
